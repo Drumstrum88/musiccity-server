c81242b9aa788485fb11d51bdfb9a7f6cd958909
drumstrum88 <rev.jsanderson@gmail.com>
2024-01-30 19:02:37 -0600

    updating post model

--- musiccityapi/models/post.py
+++ musiccityapi/models/post.py
@@ -1,7 +1,5 @@
 from django.db import models
 from musiccityapi.models.category import Category
-
-from musiccityapi.models.reaction import Reaction
 from musiccityapi.models.user import User
 
 class Post(models.Model):
@@ -10,5 +8,4 @@ class Post(models.Model):
   post_content = models.CharField(max_length=400, default='')
   created_on = models.DateTimeField(auto_now_add=True)
   image_url = models.CharField(max_length=400, default='')
-  reactions = models.ForeignKey(Reaction, on_delete=models.CASCADE, default=None)
   categories = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
