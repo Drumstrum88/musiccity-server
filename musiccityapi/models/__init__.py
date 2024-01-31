b891716997d419be81dcff6147a51f0e06102c07
drumstrum88 <rev.jsanderson@gmail.com>
2024-01-30 18:58:44 -0600

    adding models

---
+++ musiccityapi/models/__init__.py
@@ -0,0 +1,4 @@
+from post import Post
+from category import Category
+from reaction import Reaction
+from postReactions import PostReaction
---
+++ musiccityapi/models/post.py
@@ -0,0 +1,13 @@
+from django.db import models
+from musiccityapi.models.category import Category
+
+from musiccityapi.models.reaction import Reaction
+
+class Post(models.Model):
+  post_title = models.CharField(max_length=50, default='')
+  post_author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+  post_content = models.CharField(max_length=400, default='')
+  created_on = models.DateTimeField(auto_now_add=True)
+  image_url = models.CharField(max_length=400, default='')
+  reactions = models.ForeignKey(Reaction, on_delete=models.CASCADE, default=None)
+  categories = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
