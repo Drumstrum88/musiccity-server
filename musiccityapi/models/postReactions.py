ace0e489430c6853462ee49e2d47f38db634c1d4
Mara Caoile <mara.caoile@gmail.com>
2024-02-07 20:18:45 -0600

    updated counts for reactions

--- musiccityapi/models/postReactions.py
+++ musiccityapi/models/postReactions.py
@@ -9,3 +9,9 @@ class PostReaction(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
   post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None, related_name='post_reactions')
   reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE, default=None)
+  
+  @property
+  def posted_reaction(self):
+    reaction_id = self.reaction.id
+    post_id = self.post.id
+    return PostReaction.objects.filter(reaction__id=reaction_id, post__id=post_id).count()
--- musiccityapi/serializers/post_reactions.py
+++ musiccityapi/serializers/post_reactions.py
@@ -4,5 +4,5 @@ from musiccityapi.models.postReactions import PostReaction
 class PostReactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostReaction
-        fields = ['id', 'user', 'post', 'reaction']
+        fields = ['id', 'user', 'post', 'reaction', 'posted_reaction']
         depth = 1
